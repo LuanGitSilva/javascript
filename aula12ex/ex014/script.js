2fbac250b3f43d49e17317e79ab76e33cb16d8f4
Luan Silva <luan_barbosa_da_silva@outlook.com>
2022-04-29 04:55:43 -0300

    exercício 14 finalizado

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert ('Olá!') /* cria um alerta quando abre o site */
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12 ) {
+        //Bom dia!
+        img.src = 'manha.png'
+        document.body.style.background = '#b7b1d5'
+    } else if (hora >= 12 && hora < 18 ) {
+        //Boa tarde!
+        img.src = 'tarde.png'
+        document.body.style.background = '#d18a57'
+    } else {
+        //Boa noite!
+        img.src = 'noite.png'
+        document.body.style.background = '#12203b'
+    }
+}
