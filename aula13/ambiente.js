78a789350926bf735fc24bec9b5641a38b041c55
Luan Silva <luan_barbosa_da_silva@outlook.com>
2022-04-30 08:47:18 -0300

    aula 13
    estruturas de repetição
    - while
    do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1 //estrutura de repetição com teste lógico no início
+do { 
+    console.log(`Passo ${c}`)
+    c++ 
+} while (c <= 80)
+
+
+
+/* var c = 1 //estrutura de repetição com teste lógico no final
+while (c <= 6) { 
+    console.log(`Passo ${c}`)
+    c++ 
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
