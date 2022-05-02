95801b7cb860d13627657aca3cc5f0cc538f30ac
Luan Silva <luan_barbosa_da_silva@outlook.com>
2022-05-02 05:53:01 -0300

    aula 16

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'ìmpar!'
+    }
+}
+
+/*
+let res = parimpar(4)
+console.log(res)
+ou
+*/
+console.log(parimpar(7))
